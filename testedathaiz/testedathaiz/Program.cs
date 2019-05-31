6005f183dfa74db784a89c28d0424bac829deec7
jhonatan <jhonatanizidoro@gmail.com>
2019-05-31 17:17:41 -0300

    alteracao do texto

--- testedathaiz/testedathaiz/Program.cs
+++ testedathaiz/testedathaiz/Program.cs
@@ -6,7 +6,7 @@ namespace testedathaiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("teste da thaiz!");
         }
     }
 }
